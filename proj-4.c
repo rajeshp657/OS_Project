8b494e14af82104ca426579e728b2a1c43a5d7b8
dsherida <dsherida@asu.edu>
2014-04-29 00:31:37 -0700

    proj-4.c is now compiling and resembles required behavior, needs verification

--- proj-4.c
+++ proj-4.c
@@ -16,79 +16,267 @@
 
 #define BUFFER_SIZE 5
 
+/* Restrictions:
+	
+	1. Only 1 publisher
+	2. # of subscribers is NOT known in advance, new subscribers may get created while the
+			program is running. The number of subscribers is hence, dynamic.
+	3. The total number of subscribers will not exceed 5. This keeps the code simple and arrays
+			can be used to track the subscribers
+	4. If a new subscriber joins, the first item it reads is any one of the items in the queue
+			of unread items (recent past), or even the first item published in the future 
+			(your choice).
+	5. Subscribers do not terminate.
+
+*/
+/* Queue Restrictions:
+
+	1. Do not use 5 queues or multiple queues
+	2. There should only be 1 queue implemented using a circular array
+	3. Items can be integers.
+	4. All items published are to be read by all the subscribers
+	5. Items sit in the slot in the buffer until all subscribers have read it
+
+*/
+void subscribe();
+
 struct sem p;	// publish semaphore
 struct sem s;	// subscribe semaphore
 
 typedef struct {
 	int value;
+	int subs[5]; // binary array to keep track of which subscribers have or haven't consumed this item yet
+	struct Queue queue;	// a queue to hold the list of subscribers trying to read
 }item;
 
+int sc; // subscriber count, should never be greater than 5
+int swc; // subscriber waiting count, number of subscribers waiting to read an item
+int ic; // item count
+
 item buffer[BUFFER_SIZE];
-int in = 0;
-int out = 0;
-int counter = 0;
+int in = 0;		// next index to be created
+int out = 0;	// next index to be consumed
+int counter = 0;	// number of items currently in the buffer
+int loopCount = 0; // keep track of how many total items have been published
+// int subsArray[5];	// a binary to keep track of which subs are active
+
+void init_item(item* i) {
+	i->value = 1;
+	// i->subs[inValue] = 1;
+	initQueue(&i->queue);
+}
+
+void print_item(item* i) {
+	printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	// printf("\n~ ITEM");
+	printf("\n~ value = %d", i->value);
+	printf("\n~ subs  = [");
+	int x;
+	for(x=0; x<5; x++){
+		printf(" %d ", i->subs[x]);
+	}
+	printf("]");
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~");
+}
 
 void print_buffer(){
 	int i;
-	printf("\nBUFFER [ ");
+	printf("\n\n#######################");
+	printf("\n# BUFFER [ ");
 	fflush(stdout);
 
-	for(i=0;i<counter;i++){
+	for(i=0; i<BUFFER_SIZE; i++){
 		printf("%d ", buffer[i].value);
 		fflush(stdout);
 	}
 
-	printf("]\n");
+	printf("]");
+	printf("\n# counter = %d", counter);
+
+	for(i=0; i<BUFFER_SIZE; i++){
+		if(buffer[i].value != 0){
+			print_item(&buffer[i]);
+		}
+	}
+
+	printf("\n#######################");
 	fflush(stdout);
 }
+
+
+
+/*
+	Loop through the item's subs[] array and mark all active subscribers
+	need to read the item, only done on initialization of item
+	We can assume that because the item was just created, none of the
+	subscribers have read the item
+*/
+void mark_subs(item *i){
+	int x;
+	for(x=0; x<sc; x++){
+		i->subs[x] = 1;
+	}
+}
+
+/*
+	Call this method every time a subscriber loops,
+	to add itself to the list of subscribers that have yet to read an item
+	This ensures that all items created before the subscriber was created
+	are now aware that there exists a new subscriber that has yet to read it
+*/
+void update_items(int subId){
+	int x;
+	for(x=0; x<BUFFER_SIZE; x++){
+		if(buffer[x].value == 1){
+			if (buffer[x].subs[subId] == 0){
+				buffer[x].subs[subId] = 1;	// value of 1 means the subscriber with this subId has not read me yet
+			}
+		}
+	}
+}
+
+
 
 /*
 	publish:
 */
 void publish(){
 	while(1){
-		printf("\nPRODUCE");
+		printf("\n\n\t\tPUBLISH");
+		// printf("\n\t\tloopCount = %d", loopCount);
 		fflush(stdout);
 		sleep(1);
 
 		// produce an item in next_produced
-		item next_produced;
+		item next_published;
+		init_item(&next_published);
+		mark_subs(&next_published);
+		// print_item(&next_published);
 
 		while(counter == BUFFER_SIZE){
 			P(&p);
 		}
 
 		counter++;
-		buffer[in] = next_produced;
+		buffer[in] = next_published;
 		in = (in + 1) % BUFFER_SIZE;
 		
 		print_buffer();
+		// loopCount++;
 
-		V(&c);
+		V(&s);
+
+		// After 3 items have been published, start another subscriber
+		// if (loopCount == 2) {
+		// 	start_thread(subscribe);
+		// }
 	}
+}
+
+/* 
+	Method that checks whether or not the item can be removed.
+	An item may only be removed once there exists no active
+	subscribers that has yet to read the item.
+*/
+void remove_item(item *i){
+	// If the number of 2's in the subs[] array is equal to the number
+	// of subscribers, we can remove the item from the buffer
+	int twoCount = 0;
+	int x;
+
+	for(x=0; x<5; x++){
+		if(i->subs[x] == 2){
+			twoCount++;
+		}
+	}
+
+	printf("\n\t\t\ttwoCount = %d", twoCount);
+	
+	if(twoCount == sc){
+		printf("\n\t\t\tITEM REMOVED");
+		i->value=0;
+		for(x=0; x<5; x++){
+			i->subs[x] = 0;
+		}
+		counter--;
+	}	
+}
+
+/*
+	Called when a subscriber is about to read an item
+	This method needs to check to make sure no one else is modifying
+	the current item's values
+*/
+void read_item(int subId, int out){
+	// Try to read the next item in the buffer
+	printf("\n\t\t\tREADING ITEM[ %d ]", out);
+	item *next_consumed;
+	int next = (out-1)%5;
+
+	// Remove itself from the list of subscribers that have yet to read the item
+	// by flipping its index value from 1 to 2
+	if(buffer[out].value == 0){
+		// We just tried to read an empty buffer slot,
+		// so, try reading the previous item
+		// while(buffer[next].subs[subId] == 2){
+		// 	next--;
+		// }
+		next_consumed = &buffer[next];
+	} else {
+		next_consumed = &buffer[out];
+	}
+
+	if(next_consumed->value == 1){
+		next_consumed->subs[subId] = 2; // value of 2 means that the subscriber with its index equal to subId has now read the item
+	}
+
+	remove_item(next_consumed);
 }
 
 /*
 	subscribe:
 */
 void subscribe(){
+	int subscriberID = sc;	// give each subscriber an ID
+	sc++;
+	int nextRead = out; // integer that keeps track of which item index was just read
+
 	while(1) {
-		printf("\nCONSUME");
+		printf("\n\n\t\tSUBSCRIBER[ %d ]", subscriberID);
+		printf("\n\t\tloopCount = %d", loopCount);
+		printf("\n\t\tsc = %d", sc);
 		fflush(stdout);
 		sleep(1);
 
-		item next_consumed;
+		// Update all items in buffer,
+		// let them know that this thread hasn't read them yet
+		update_items(subscriberID);
+
+		// item next_consumed;
+		// item temp;
 
 		while(counter == 0) {
-			P(&c);
+			P(&s);
 		}
 
-		counter--;
-		next_consumed = buffer[out];
+		// Consume an item, "read the item"
+		read_item(subscriberID, nextRead);
+		nextRead = (nextRead+1)%5;
+
+		// counter--;
+		// next_consumed = buffer[out];
+		// buffer[out] = temp;
 		out = (out + 1) % BUFFER_SIZE;
+		
 		print_buffer();
+		loopCount++;
 		
 		V(&p);
+
+		// After a few items (2) the subscriber starts another subscriber
+		if (loopCount == 2 || loopCount == 3 || loopCount == 4 || loopCount == 5) {
+			start_thread(subscribe);
+		}
 	}
 }
 
@@ -101,46 +289,12 @@ void idle(){
 	}
 }
 
-/* Restrictions:
-	
-	1. Only 1 publisher
-	
-	2. # of subscribers is NOT known in advance, new subscribers may get created while the
-		program is running. The number of subscribers is hence, dynamic.
-	
-	3. The total number of subscribers will not exceed 5. This keeps the code simple and arrays 			can be used to track the subscribers
-
-	4. If a new subscriber joins, the first item it reads is any one of the items in the queue 			of unread items (recent past), or even the first item published in the future 
-		(your choice).
-
-	5. Subscribers do not terminate.
-
-*/
-
-/* Queue Restrictions:
-
-	1. Do not use 5 queues or multiple queues
-
-	2. There should only be 1 queue implemented using a circular array
-
-	3. Items can be integers.
-
-	4. All items published are to be read by all the subscribers
-
-	5. Items sit in the slot in the buffer until all subscribers have read it
-
-*/
-
-
-
 /* Main program:
 
 	1. Main program starts a publisher and then starts a subscriber
-
 	2. After a few items the publisher starts another subscriber
-
-	3. This continues until 5 subscribers have been created and no further ones are created
-
+	3. This continues until 5 subscribers have been created and no 
+			further ones are created
 */
 int main() {
 	printf("\n>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<\n");
@@ -149,7 +303,7 @@ int main() {
 
 	// Initialize the semaphores
 	InitSem(&p, 1);
-	InitSem(&c, 1);
+	InitSem(&s, 1);
 
 	// initialize ReadyQ
 	printf("\n>>  I  <<\n    INITIALIZING READY QUEUE :\n>>  I  <<\n");
@@ -165,7 +319,9 @@ int main() {
 
 	// start the subscriber
 	printf("\n>>  S  <<\n    STARTING SUBSCRIBER. ...\n>>  S  <<\n");
-	start_thread(publish);
+	start_thread(subscribe);
+
+	run();
 	
-return 0;
+	return 0;
 }
