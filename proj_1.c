42536ec11f1529dd52d6b022a5db30dadac506f7
rajeshp657 <82557773+rajeshp657@users.noreply.github.com>
2022-07-07 12:08:09 -0700

    Update proj_1.c

--- proj_1.c
+++ proj_1.c
@@ -1,19 +1,5 @@
 /*
- +--------------------------------------------------+
- | Filename: proj_1.c                               |
- +--------------------------------------------------+
- |           CSE 430, Programming Project 1         |
- |               Dasgupta T, Th 4:30 pm             |
- |                   March 27, 2014                 |
- |                                                  |
- |          Group: Doug Sheridan, 1202789648        |
- |                 Amy Mazzola,                     |
- +--------------------------------------------------+
- | Description:                                     |
- |    This program is used to implement test        |
- |    case scenarios in order to prove that         |
- |    all requirements on q.hpp are met.            |
- +--------------------------------------------------+
+
  */
 //#include "q.h" // already included in proj-2.c
 
